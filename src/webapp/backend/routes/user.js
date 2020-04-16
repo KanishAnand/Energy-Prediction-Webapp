6e9fc9373a80f83cdddcdb994589da39518c7363
Akshat Goyal <akshatgoyalak23@gmail.com>
2020-04-16 14:35:44 +0530

    form api added to routes, sends query form to maintainers

--- src/webapp/backend/routes/user.js
+++ src/webapp/backend/routes/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 let User = require("./../models/user");
+let nodemailer = require("nodemailer");
 
 // Gets all the users
 router.route("/").get(function (req, res) {
@@ -70,6 +71,64 @@ router.route("/exist").post(function (req, res) {
         res.status(400).json(err);
       });
   }
+});
+
+// sends query form to all the maintainers
+router.route("/form").post(function (req, res) {
+  User.find({ userType: "maintenance team" })
+    .then((users) => {
+      let to = "";
+      for (let i = 0; i < users.length; i++) {
+        if (i) {
+          to += ", ";
+        }
+        to += users[i]["email"];
+      }
+      if (to === "") {
+        res.status(400).json({ message: "Maintenance Team not available!" });
+      }
+      User.findOne({ username: req.body.username })
+        .then((user) => {
+          if (user === null) {
+            res.status(400).json({
+              message: "Username: " + req.body.username + " does not exist!",
+            });
+          }
+          let text = req.body.text;
+          text += "\n\nSend By:";
+          text += "\nName: " + user["firstName"] + " " + user["lastName"];
+          text += "\nEmail: " + user["email"];
+          text += "\nUser Type: " + user["userType"];
+          let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: "",
+              pass: "",
+            },
+          });
+          let mailOptions = {
+            from: "",
+            to: to,
+            subject: req.body.subject,
+            text: text,
+          };
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              res
+                .status(400)
+                .json({ message: "Username and Password not accepted" });
+            } else {
+              res.status(200).json("Email sent: " + info.response);
+            }
+          });
+        })
+        .catch((err) => {
+          res.status(400).json(err);
+        });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 // modify user's info
