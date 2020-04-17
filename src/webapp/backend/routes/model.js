de88f94e2a147fb97bb9946b8d9390b439302c25
Akshat Goyal <akshatgoyalak23@gmail.com>
2020-04-17 15:11:43 +0530

    predict function modified to predict for a range

--- src/webapp/backend/routes/model.js
+++ src/webapp/backend/routes/model.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
 
 // returns energy prediction
-router.route("/predict").post(function(req, res) {
+router.route("/predict").post(function (req, res) {
   try {
     const { spawn } = require("child_process");
     const process = spawn("python3", [
       "./routes/model.py",
-      req.body.date,
-      req.body.time
+      "predict",
+      req.body.fromDate,
+      req.body.fromTime,
+      req.body.toDate,
+      req.body.toTime,
     ]);
-    process.stdout.on("data", data => {
-      res.status(200).end(data.toString());
+    process.stdout.on("data", (data) => {
+      let plist = require("../models/plist.json");
+      res.status(200).send(plist);
     });
   } catch (err) {
     res.status(404).json(err);
--- src/webapp/backend/routes/model.py
+++ src/webapp/backend/routes/model.py
@@ -1,17 +1,42 @@
 import sys
 import pickle
 import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+import json
 
 
 def predict(model):
-    data = [[sys.argv[1] + " " + sys.argv[2]]]
-    test = pd.DataFrame(data, columns=['ds'])
-    val = model.predict(test).to_dict()
-    print(str(round(val['yhat'][0], 2)))
+    From = datetime.strptime(sys.argv[2] + " " + sys.argv[3], '%Y-%m-%d %H:%M')
+    To = datetime.strptime(sys.argv[4] + " " + sys.argv[5], '%Y-%m-%d %H:%M')
+    plist = []
+    dayTime = From
+    data = pd.DataFrame([dayTime], columns=['ds'])
+    val = model.predict(data).to_dict()
+    plist.append(
+        {"date": str(dayTime.strftime('%Y-%m-%d')), "time": str(dayTime.strftime('%H:%M:%S')), "yhat": round(np.exp(val['yhat'][0]), 2)})
+    dayTime = dayTime.replace(minute=0) + timedelta(hours=1)
+    while dayTime <= To:
+        data = pd.DataFrame([dayTime], columns=['ds'])
+        val = model.predict(data).to_dict()
+        plist.append(
+            {"date": str(dayTime.strftime('%Y-%m-%d')), "time": str(dayTime.strftime('%H:%M:%S')), "yhat": round(np.exp(val['yhat'][0]), 2)})
+        dayTime += timedelta(hours=1)
+    if To != From and To.minute != 0:
+        dayTime = To
+        data = pd.DataFrame([dayTime], columns=['ds'])
+        val = model.predict(data).to_dict()
+        plist.append(
+            {"date": str(dayTime.strftime('%Y-%m-%d')), "time": str(dayTime.strftime('%H:%M:%S')), "yhat": round(np.exp(val['yhat'][0]), 2)})
+    with open("./models/plist.json", "w") as f:
+        json.dump(plist, f)
+    print("Energy Prediction Completed Successfully!")
     sys.stdout.flush()
+    return
 
 
 if __name__ == "__main__":
     with open('./models/model.pkl', 'rb') as f:
         model = pickle.load(f)
-        predict(model)
+        if sys.argv[1] == "predict":
+            predict(model)
