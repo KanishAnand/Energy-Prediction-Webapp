e9fa4f31dad28fbe2d9d9381e28f0a702fc4e9bb
KanishAnand <therealkanish@gmail.com>
2020-04-17 13:36:08 +0530

    moved font folder to components

--- src/webapp/frontend/src/components/Day-card.component.js
+++ src/webapp/frontend/src/components/Day-card.component.js
@@ -1,30 +1,42 @@
 import React from "react";
+import "./Card.css";
+import "./owfont-master/css/owfont-regular.css";
 var moment = require("moment");
 
-const DayCard = ({ reading }) => {
-	let newDate = new Date();
-	const weekday = reading.dt * 1000;
-	newDate.setTime(weekday);
+class Card extends React.Component {
+	// Props: day, key(index)
 
-	const imgURL = `owf owf-${reading.weather[0].id} owf-5x`;
+	render() {
+		let newDate = new Date();
+		const weekday = this.props.day.dt * 1000;
+		newDate.setTime(weekday);
 
-	return (
-		<div className="col-sm-2">
-			<div className="card">
-				<h3 className="card-title">{moment(newDate).format("dddd")}</h3>
-				<p className="text-muted">
-					{moment(newDate).format("MMMM Do, h:mm a")}
-				</p>
-				<i className={imgURL}></i>
-				<h2>{Math.round(reading.main.temp)} °F</h2>
-				<div className="card-body">
-					<p className="card-text">
-						{reading.weather[0].description}
+		const imgURL =
+			"owf owf-" + this.props.day.weather[0].id + " owf-5x red";
+
+		return (
+			<div className="col-auto">
+				<div className="card bg-light">
+					<h3 className="card-title">
+						{moment(newDate).format("dddd")}
+					</h3>
+					<p className="text-muted">
+						{moment(newDate).format("MMMM Do, h:mm a")}
 					</p>
+					<i className={imgURL}></i>
+					<h2>{Math.round(this.props.day.main.temp)} °F</h2>
+					<div className="card-body">
+						<p className="card-text">
+							{this.props.day.weather[0].description}
+						</p>
+						<button className="btn btn-dark btn-outline-light">
+							See Hourly Forecast
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
-export default DayCard;
+export default Card;
--- src/webapp/frontend/src/components/view-weather.component.js
+++ src/webapp/frontend/src/components/view-weather.component.js
@@ -1,35 +1,28 @@
 import React, { Component } from "react";
-import DayCard from "./Day-card.component";
+import Card from "./Day-card.component";
+const weatherURL =
+	"http://api.openweathermap.org/data/2.5/forecast?id=1269843&appid=95e286bae5647877dbb924f3779736a8&units=imperial";
 
 export default class Weather extends Component {
 	state = {
-		fullData: [],
-		dailyData: [],
+		days: [],
 	};
 
 	componentDidMount = () => {
-		const weatherURL =
-			"http://api.openweathermap.org/data/2.5/forecast?id=1269843&appid=95e286bae5647877dbb924f3779736a8&units=imperial";
-
 		fetch(weatherURL)
 			.then((res) => res.json())
 			.then((data) => {
+				console.log("Data List Loaded", data.list);
 				const dailyData = data.list.filter((reading) =>
 					reading.dt_txt.includes("18:00:00")
 				);
-				this.setState(
-					{
-						fullData: data.list,
-						dailyData: dailyData,
-					},
-					() => console.log(this.state)
-				);
+				this.setState({ days: dailyData });
 			});
 	};
 
-	formatDayCards = () => {
-		return this.state.dailyData.map((reading, index) => (
-			<DayCard reading={reading} key={index} />
+	formatCards = () => {
+		return this.state.days.map((day, index) => (
+			<Card day={day} key={index} />
 		));
 	};
 
@@ -37,9 +30,9 @@ export default class Weather extends Component {
 		return (
 			<div className="container">
 				<h1 className="display-1 jumbotron">5-Day Forecast.</h1>
-				<h5 className="display-5 text-muted">Hyderabad, India</h5>
+				<h5 className="display-5 text-muted">New York, US</h5>
 				<div className="row justify-content-center">
-					{this.formatDayCards()}
+					{this.formatCards()}
 				</div>
 			</div>
 		);
