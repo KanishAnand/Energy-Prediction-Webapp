7fad92954e73e112cd955be7a39e91375eb11f08
KanishAnand <therealkanish@gmail.com>
2020-04-18 01:21:53 +0530

    renamed day card classes

--- src/webapp/frontend/src/components/Dashboard.component.js
+++ src/webapp/frontend/src/components/Dashboard.component.js
@@ -104,7 +104,7 @@ export default class Dashboard extends Component {
 				{/* static navbar - top */}
 				<Nav className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
 					<Container className="navbar-brand h1 mb-0 text-large font-medium">
-						Energy Consumption Dashboard
+						Online Retail Dashboard
 					</Container>
 					<Container className="navbar-nav ml-auto">
 						<Container className="user-detail-section">
@@ -142,12 +142,13 @@ export default class Dashboard extends Component {
 							<Container className="card grid-card is-card-dark">
 								<Container className="card-heading">
 									<Container className="is-dark-text-light letter-spacing text-small">
-										Total Energy Consumption
+										Total Revenue
 									</Container>
 								</Container>
 
 								<Container className="card-value pt-4 text-x-large">
-									{this.state.totalenergy}
+									<span className="text-large pr-1">$</span>
+									{this.state.totalRevenue}
 								</Container>
 							</Container>
 						</Container>
@@ -156,7 +157,7 @@ export default class Dashboard extends Component {
 							<Container className="card grid-card is-card-dark">
 								<Container className="card-heading">
 									<Container className="is-dark-text-light letter-spacing text-small">
-										Total Expenditure
+										Revenue from Amazon
 									</Container>
 									<Container className="card-heading-brand">
 										<i className="fab fa-amazon text-large" />
@@ -164,8 +165,8 @@ export default class Dashboard extends Component {
 								</Container>
 
 								<Container className="card-value pt-4 text-x-large">
-									<span className="text-large pr-1">₹</span>
-									{this.state.totalexpenditure}
+									<span className="text-large pr-1">$</span>
+									{this.state.amRevenue}
 								</Container>
 							</Container>
 						</Container>
@@ -174,36 +175,57 @@ export default class Dashboard extends Component {
 							<Container className="card grid-card is-card-dark">
 								<Container className="card-heading">
 									<Container className="is-dark-text-light letter-spacing text-small">
-										Total Energy Consumption
-									</Container>
-								</Container>
-
-								<Container className="card-value pt-4 text-x-large">
-									{this.state.totalenergy}
-								</Container>
-							</Container>
-						</Container>
-
-						<Container className="col-lg-3 col-sm-6 is-light-text mb-4">
-							<Container className="card grid-card is-card-dark">
-								<Container className="card-heading">
-									<Container className="is-dark-text-light letter-spacing text-small">
-										Total Expenditure
+										Revenue from Ebay
 									</Container>
 									<Container className="card-heading-brand">
-										<i className="fab fa-amazon text-large" />
+										<i className="fab fa-ebay text-x-large logo-adjust" />
 									</Container>
 								</Container>
 
 								<Container className="card-value pt-4 text-x-large">
-									<span className="text-large pr-1">₹</span>
-									{this.state.totalexpenditure}
+									<span className="text-large pr-1">$</span>
+									{this.state.ebRevenue}
+								</Container>
+							</Container>
+						</Container>
+
+						<Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+							<Container className="card grid-card is-card-dark">
+								<Container className="card-heading">
+									<Container className="is-dark-text-light letter-spacing text-small">
+										Revenue from Etsy
+									</Container>
+									<Container className="card-heading-brand">
+										<i className="fab fa-etsy text-medium" />
+									</Container>
+								</Container>
+
+								<Container className="card-value pt-4 text-x-large">
+									<span className="text-large pr-1">$</span>
+									{this.state.etRevenue}
 								</Container>
 							</Container>
 						</Container>
 					</Container>
 
+					{/* row 2 - conversion */}
 					<Container className="row">
+						<Container className="col-md-4 col-lg-3 is-light-text mb-4">
+							<Container className="card grid-card is-card-dark">
+								<Container className="card-heading mb-3">
+									<Container className="is-dark-text-light letter-spacing text-small">
+										Product Views
+									</Container>
+								</Container>
+								<Container className="card-value pt-4 text-x-large">
+									{this.state.productViews}
+									<span className="text-medium pl-2 is-dark-text-light">
+										views
+									</span>
+								</Container>
+							</Container>
+						</Container>
+
 						<Container className="col-md-8 col-lg-9 is-light-text mb-4">
 							<Container className="card is-card-dark chart-card">
 								<Container className="row full-height">
@@ -211,37 +233,115 @@ export default class Dashboard extends Component {
 										<Container className="chart-container full-height">
 											<ReactFC
 												{...{
-													type: "column2d",
-													width: 600,
-													height: 400,
+													type: "doughnut2d",
+													width: "100%",
+													height: "100%",
 													dataFormat: "json",
 													containerBackgroundOpacity:
 														"0",
 													dataSource: {
 														chart: {
 															caption:
-																"Yearly Energy Consumption",
-															numberSuffix:
-																"Kw/hr",
+																"Purchase Rate",
+															theme: "ecommerce",
+															defaultCenterLabel: `${this.state.purchaseRate}%`,
+															paletteColors:
+																"#3B70C4, #000000",
 														},
 														data: [
 															{
-																label:
-																	"January",
-																value: "800",
+																label: "active",
+																value: `${this.state.purchaseRate}`,
 															},
 															{
 																label:
-																	"February",
-																value: "730",
+																	"inactive",
+																alpha: 5,
+																value: `${
+																	100 -
+																	this.state
+																		.purchaseRate
+																}`,
+															},
+														],
+													},
+												}}
+											/>
+										</Container>
+									</Container>
+									<Container className="col-sm-4 full-height border-left border-right">
+										<Container className="chart-container full-height">
+											<ReactFC
+												{...{
+													type: "doughnut2d",
+													width: "100%",
+													height: "100%",
+													dataFormat: "json",
+													containerBackgroundOpacity:
+														"0",
+													dataSource: {
+														chart: {
+															caption:
+																"Checkout Rate",
+															theme: "ecommerce",
+															defaultCenterLabel: `${this.state.checkoutRate}%`,
+															paletteColors:
+																"#41B6C4, #000000",
+														},
+														data: [
+															{
+																label: "active",
+																value: `${this.state.checkoutRate}`,
 															},
 															{
-																label: "March",
-																value: "590",
+																label:
+																	"inactive",
+																alpha: 5,
+																value: `${
+																	100 -
+																	this.state
+																		.checkoutRate
+																}`,
+															},
+														],
+													},
+												}}
+											/>
+										</Container>
+									</Container>
+									<Container className="col-sm-4 full-height">
+										<Container className="chart-container full-height">
+											<ReactFC
+												{...{
+													type: "doughnut2d",
+													width: "100%",
+													height: "100%",
+													dataFormat: "json",
+													containerBackgroundOpacity:
+														"0",
+													dataSource: {
+														chart: {
+															caption:
+																"Abandoned Cart Rate",
+															theme: "ecommerce",
+															defaultCenterLabel: `${this.state.abandonedRate}%`,
+															paletteColors:
+																"#EDF8B1, #000000",
+														},
+														data: [
+															{
+																label: "active",
+																value: `${this.state.abandonedRate}`,
 															},
 															{
-																label: "April",
-																value: "520",
+																label:
+																	"inactive",
+																alpha: 5,
+																value: `${
+																	100 -
+																	this.state
+																		.abandonedRate
+																}`,
 															},
 														],
 													},
@@ -253,7 +353,80 @@ export default class Dashboard extends Component {
 							</Container>
 						</Container>
 					</Container>
+
+					{/* row 3 - orders trend */}
+					<Container className="row" style={{ minHeight: "400px" }}>
+						<Container className="col-md-6 mb-4">
+							<Container className="card is-card-dark chart-card">
+								<Container className="chart-container large full-height">
+									<ReactFC
+										{...{
+											type: "bar2d",
+											width: "100%",
+											height: "100%",
+											dataFormat: "json",
+											containerBackgroundOpacity: "0",
+											dataEmptyMessage: "Loading Data...",
+											dataSource: {
+												chart: {
+													theme: "ecommerce",
+													caption: "Orders Trend",
+													subCaption: "By Stores",
+												},
+												data: this.state
+													.ordersTrendStore,
+											},
+										}}
+									/>
+								</Container>
+							</Container>
+						</Container>
+
+						<Container className="col-md-6 mb-4">
+							<Container className="card is-card-dark chart-card">
+								<Container className="chart-container large full-height">
+									<ReactFC
+										{...{
+											type: "usaregion",
+											width: "100%",
+											height: "100%",
+											dataFormat: "json",
+											containerBackgroundOpacity: "0",
+											dataEmptyMessage: "Loading Data...",
+											dataSource: {
+												chart: {
+													theme: "ecommerce",
+													caption: "Orders Trend",
+													subCaption: "By Region",
+												},
+												colorrange: {
+													code: "#F64F4B",
+													minvalue: "0",
+													gradient: "1",
+													color: [
+														{
+															minValue: "10",
+															maxvalue: "25",
+															code: "#EDF8B1",
+														},
+														{
+															minvalue: "25",
+															maxvalue: "50",
+															code: "#18D380",
+														},
+													],
+												},
+												data: this.state
+													.ordersTrendRegion,
+											},
+										}}
+									/>
+								</Container>
+							</Container>
+						</Container>
+					</Container>
 				</Container>
+				{/* content area end */}
 			</Container>
 		);
 	}
