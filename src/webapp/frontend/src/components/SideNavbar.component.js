035b937347b0901af769ef54f4683d909a61eec7
KanishAnand <therealkanish@gmail.com>
2020-04-18 19:29:07 +0530

    side navbar aligned properly

--- src/webapp/frontend/src/components/SideNavbar.component.js
+++ src/webapp/frontend/src/components/SideNavbar.component.js
@@ -1,20 +1,24 @@
 //SideNav.js
 import React, { Component } from "react";
-// import Paper from "material-ui/Paper";
-// import Divider from "material-ui/Divider";
-// import FlatButton from "material-ui/FlatButton";
-
 import "../css/SideNavbar.css";
-// import avi from "../css/logo.ico";
+import logo from "../images/logo.png";
 
 class SideNavbar extends Component {
 	render() {
 		return (
 			<div class="sidenav" align="center">
-				<a href="#">About</a>
-				<a href="#">Services</a>
-				<a href="#">Clients</a>
-				<a href="#">Contact</a>
+				<div className="sideContent">
+					<img src={logo} alt="Organisation"></img>
+					<a style={{ paddingTop: "15%" }} href="#">
+						Home
+					</a>
+					<a href="#">Prediction</a>
+					<a href="#">Weather</a>
+					<a href="#">Profile</a>
+					<a href="#">Users</a>
+					<a href="#">Customer Care</a>
+					<a href="#">Logout</a>
+				</div>
 			</div>
 		);
 	}
