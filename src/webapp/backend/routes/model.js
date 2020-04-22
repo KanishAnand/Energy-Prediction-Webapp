fa48597e55adb5f7ef718de87a40b0368838eaa4
KanishAnand <therealkanish@gmail.com>
2020-04-22 11:50:23 +0530

    all tests working

--- src/webapp/backend/routes/model.js
+++ src/webapp/backend/routes/model.js
@@ -4,44 +4,44 @@ const fs = require("fs");
 
 // returns predicted energy value for given time range
 router.route("/predict").post(function (req, res) {
-  try {
-    const process = spawn("python3", [
-      "./models/model.py",
-      "predict",
-      req.body.fromDate,
-      req.body.fromTime,
-      req.body.toDate,
-      req.body.toTime,
-    ]);
-    process.stdout.on("data", (data) => {
-      let fileName = data.toString().split("\n")[0];
-      let output = require("../models/" + fileName);
-      delete require.cache[require.resolve("../models/" + fileName)];
-      fs.unlinkSync("./models/" + fileName);
-      res.status(200).send(output);
-    });
-  } catch (err) {
-    res.status(404).json(err);
-  }
+	try {
+		const process = spawn("python3", [
+			"./models/model.py",
+			"predict",
+			req.body.fromDate,
+			req.body.fromTime,
+			req.body.toDate,
+			req.body.toTime,
+		]);
+		process.stdout.on("data", (data) => {
+			let fileName = data.toString().split("\n")[0];
+			let output = require("../models/" + fileName);
+			delete require.cache[require.resolve("../models/" + fileName)];
+			fs.unlinkSync("./models/" + fileName);
+			res.status(200).send(output);
+		});
+	} catch (err) {
+		res.status(404).json(err);
+	}
 });
 
 // returns graph of the predicted energy values for given time range
 router.route("/graph").post(function (req, res) {
-  try {
-    const process = spawn("python3", [
-      "./models/model.py",
-      "graph",
-      req.body.fromDate,
-      req.body.fromTime,
-      req.body.toDate,
-      req.body.toTime,
-    ]);
-    process.stdout.on("data", (data) => {
-      res.status(200).send(data.toString().split("\n")[0]);
-    });
-  } catch (err) {
-    res.status(404).json(err);
-  }
+	try {
+		const process = spawn("python3", [
+			"./models/model.py",
+			"graph",
+			req.body.fromDate,
+			req.body.fromTime,
+			req.body.toDate,
+			req.body.toTime,
+		]);
+		process.stdout.on("data", (data) => {
+			res.status(200).send(data.toString().split("\n")[0]);
+		});
+	} catch (err) {
+		res.status(404).json(err);
+	}
 });
 
 module.exports = router;
