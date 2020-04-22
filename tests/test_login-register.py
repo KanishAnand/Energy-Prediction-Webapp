8c2d9356410102adfc60dd60433880bf43c2723c
KanishAnand <therealkanish@gmail.com>
2020-04-22 12:29:37 +0530

    login register test bug solved

--- tests/test_login-register.py
+++ tests/test_login-register.py
@@ -2,20 +2,33 @@ import pytest
 import requests
 import json
 
-# def test_register():
-#     url = "http://localhost:4000/user/add"
-#     headers = {'Content-Type': 'application/json'}
-#     user = {"username": "J3o22hn",
-#             "email": "joh312n@gmail.com",
-#             "phoneNo": "3918187776",
-#             "firstName": "John",
-#             "lastName": "Cena",
-#             "password": "Hello@123",
-#             "confirmPassword": "Hello@123",
-#             "userType": "Owner"
-#             }
-#     resp = requests.post(url, data=json.dumps(user), headers=headers)
-#     assert resp.status_code == 200
+
+def test_register():
+    url = "http://localhost:4000/user/exist"
+    headers = {'Content-Type': 'application/json'}
+    user = {"username": "testing",
+            "email": "testing@gmil.com",
+            "phoneNo": "4445556669",
+            }
+
+    resp = requests.post(url, data=json.dumps(user), headers=headers)
+    assert resp.status_code == 200
+
+    data = resp.text
+    if data == "{}":
+        url = "http://localhost:4000/user/add"
+        headers = {'Content-Type': 'application/json'}
+        user = {"username": "testing",
+                "email": "testing@gmail.com",
+                "phoneNo": "4445556669",
+                "firstName": "testing",
+                "lastName": "testing",
+                "password": "Hello@123",
+                "confirmPassword": "Hello@123",
+                "userType": "Owner"
+                }
+        resp = requests.post(url, data=json.dumps(user), headers=headers)
+        assert resp.status_code == 200
 
 
 def test_login():
